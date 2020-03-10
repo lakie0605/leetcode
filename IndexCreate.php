fef88a07bf17526ab7f1b74b7eabc51661593e57
lakie0605 <lakie0605@163.com>
2020-03-10 16:11:35 +0800

    格式化

---
+++ IndexCreate.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lakie
+ * Date: 2020/3/10
+ * Time: 11:57
+ */
+class IndexCreate
+{
+    private $root;
+    private $text;
+    public function __construct()
+    {
+        $this->root = '.';
+        $this->text = "# Leetcode解题记录\n ### 目录:\n\n";
+    }
+
+    public function makeIndex()
+    {
+        $this->text = $this->LoopDir($this->root);
+        $readme = 'README.MD';
+        $file = fopen($readme, 'w+');
+        fwrite($file, $this->text);
+        fclose($file);
+    }
+
+    public function LoopDir($dir)
+    {
+        $tierNum = substr_count($dir, '/');
+        $retract = '';
+        if ($tierNum > 0) {
+            for ($n = 0; $n < $tierNum; $n++) {
+                $retract .= "  ";
+            }
+            $retract .= '+ ';
+        }
+        $dirList = scandir($dir);
+        array_multisort($dirList, SORT_NATURAL);
+        for ($i = 0; $i < count($dirList); $i++) {
+            if (is_dir($dir . '/' . $dirList[$i]) && substr($dirList[$i], 0, 1) != '.') {
+                $this->text .= $retract . '[' . $dirList[$i] . ']' . '(' . $dir . '/' . $dirList[$i] . ')' . "\n\n";
+                $this->LoopDir($dir. '/' . $dirList[$i]);
+            }
+        }
+        return $this->text;
+    }
+}
+$I = new IndexCreate();
+$I->makeIndex();
